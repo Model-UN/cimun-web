7ef12b25526141e5974f4619c6bf72fed742195f
James He <hejames98@gmail.com>
2021-06-13 17:56:38 -0500

    Use Link closing tag

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <Link href="http://cimun.org">CIMUN XVIII</a>!
+          Welcome to <Link href="http://cimun.org">CIMUN XVIII</Link>!
         </h1>
 
         <p className={styles.description}>
