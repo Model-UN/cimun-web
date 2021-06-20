864a7d11c0adc4d81de47c80451c08ba6bc23619
James He <hejames98@gmail.com>
2021-06-20 14:21:14 -0500

    Dev/1 jh deployment rebase (#27)
    * Dev/2 vercel sponsorship (#20) (#25) (#26)
    
    * Add LICENSE
    
    * Update gitignore
    
    * Update vercel footer
    
    * Add Vercel banner to README
    
    * Add divider for style points
    
    * Update links and copy Deploy on Vercel README section
    
    * Small README adjustment
    
    * created the jumbotron component for the landing of the home page and put it in a components folder and imported it into pages/index, set up fonts and colors files and some early global styles but those will be changed over to styled components based off a decision after this code was written (#22)
    
    * installed and set up some basic styled-components, converted parts of jumbotron component and main page ot use some styled components (#24)
    
    * Use utm params for vercel redirects
    
    * Readme changes
    
    Co-authored-by: sdmungov <55511406+sdmungov@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://vercel.com?utm_source=model-un&utm_campaign=oss"
             className={styles.card}
           >
             <h3>Committees &rarr;</h3>
@@ -55,12 +55,12 @@ export default function Home() {
       </ComponentWrapper>
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://vercel.com?utm_source=model-un&utm_campaign=oss"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span className={styles.vercel}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
