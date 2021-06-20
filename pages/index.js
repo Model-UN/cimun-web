7139aef3290d89bd693b0b0a2d09c8968dcffee0
James He <hejames98@gmail.com>
2021-06-20 11:30:54 -0500

    Dev/2 vercel sponsorship (#20) (#25)
    * Add LICENSE
    
    * Update gitignore
    
    * Update vercel footer
    
    * Add Vercel banner to README
    
    * Add divider for style points
    
    * Update links and copy Deploy on Vercel README section
    
    * Small README adjustment

--- pages/index.js
+++ pages/index.js
@@ -55,12 +55,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://vercel.com?utm_source=model-un&utm_campaign=oss"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span className={styles.vercel}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
