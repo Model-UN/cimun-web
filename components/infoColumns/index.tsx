f7ac956675ce74e82d9cabbc5b841574ed540ff6
James He <hejames98@gmail.com>
2021-07-02 21:35:29 -0500

    Dev/17 jh info cards (#36)
    * Defeat Megatron (#34)
    
    * Switch out hero svg for image
    
    * Defeated megatron
    
    * Incredibly unnecessary header styling
    
    * Small style and copy changes
    
    * Adjust per comments
    
    * Adjust per comments
    
    * Adjust mobile view and justify text
    
    * Somehow enhanced the margins
    
    * Add extra title breakpoint
    
    * I love this breakpoint function - Update subtitle for mobile
    
    * Adjust brightness of jumbotron in a scuffed manner
    
    * Add breakpoint for SubTitle width

--- components/infoColumns/index.tsx
+++ components/infoColumns/index.tsx
@@ -2,7 +2,6 @@ import { ComponentWrapper } from "../../styles/containers";
 import { SubTitle, Body } from "../../styles/typography";
 import { colors } from "../../styles/colors";
 import { breakpoints } from "../../styles/breakpoints";
-import Link from "next/link";
 import styled from "styled-components";
 
 const InfoColumnWrapper = styled(ComponentWrapper)`
@@ -56,35 +55,36 @@ const CardContainer = styled.div<{
   background-color: ${(props) => (props.color ? props.color : "transparent")};
   margin: ${(props) => (props.margins ? props.margins : "1rem")};
   padding: ${(props) => (props.pad ? props.pad : "1.5rem")};
-  ${breakpoints("padding", "", [{ 1400: "1.5rem auto" }])};
+  ${breakpoints("padding", "", [{ 1400: "1rem" }])};
   ${breakpoints("margin", "", [{ 1400: "1rem 0" }])};
 `;
 const Title = styled(SubTitle)`
-  font-size: 4rem;
-  ${breakpoints("font-size", "", [{ 1000: "2.5rem" }])};
+  font-size: 6rem;
+  ${breakpoints("font-size", "", [{ 1000: "4rem" }, { 720: "2.25rem" }])};
   width: 100px;
   ${breakpoints("width", "", [{ 1000: "100%" }])};
   margin: 0;
+  font-weight: 1000;
 `;
 
 const InfoColumns = () => {
   return (
     <InfoColumnWrapper>
-      <CardContainer flex="1" margins="1rem 0" pad="1rem 1rem 1rem 0">
-        <Title>About CIMUN</Title>
-        <Body size="1rem" line={1.4}>
+      <CardContainer flex="1" margins="1rem 0" pad="0 1rem 0 0">
+        <Title line={0.9}>Welcome Back, Delegates.</Title>
+        <Body line={1.4} align={"justify"}>
           For 18 years, CIMUN has been a premier conference for scholastic Model
           United Nations programs. <br />
           <br />
           Beginning with our opening ceremonies, delegates are introduced to a
-          24/7, fully-integrated simulation, where they are challenged to work
+          fully-integrated simulation, where they are challenged to work
           across committees as they confront complex present-day and historical
           issues. <br />
           <br />
-          The CIMUN Philosophy believes that educational value is defined by
-          realism. In this regard, action taken in one committee affects the
+          The CIMUN Philosophy is to educate through <em>accuracy and realism</em>.
+          In this regard, action taken in one committee affects the
           proceedings in every other committee. Crises occur in real-time
-          throughout the conference, and CIMUN staff diligently work to create a
+          throughout the weekend, and CIMUN staff diligently work to create a
           conference that is fast-paced, nuanced, and above all else,
           educational. CIMUN recognizes that nations cannot always solve
           problems together, and that the complexity of international affairs is
@@ -98,17 +98,28 @@ const InfoColumns = () => {
           post-graduates, and professionals from backgrounds in academia,
           finance, politics, business, technology and more to deliver a
           high-caliber educational experience to our delegates.
+          <br />
+          <br />
+          This coming year, we welcome you - delegates, faculty, sponsors, and all others -
+          <em> in-person</em>, to the <strong> 18th Chicago International Model United Nations
+          conference</strong>. We hope you can make it!
         </Body>
       </CardContainer>
-      <CardWrapper flex="2" margins="1rem 0" pad="1rem 0 1rem 1rem">
+      <CardWrapper flex="2" margins="1rem 0" pad="1rem 0 6rem 1rem">
         <CardContainer
           bordered
           height="600px"
-          color={colors.fadedPrimaryBlue}
+          color={colors.primaryBlue}
           margins="1rem 1rem 0 0"
         >
-          <SubTitle size="2.5rem" margins="20px 0" self="center">
-            Join Us!
+          <SubTitle size="3.5rem"
+                    mobSize="2rem"
+                    margins="10px 0"
+                    self="center"
+                    align="center"
+                    line={0.9}
+                    color={colors.ltGray}>
+            Join us in-person!
           </SubTitle>
           <div
             style={{
@@ -116,24 +127,27 @@ const InfoColumns = () => {
               justifyContent: "center",
             }}
           >
-            <Body align="center" self="center" size="1.2rem">
-              <b>February 3rd - 6th 2022</b>
+            <Body align="center" self="center" size="2rem" weight={700} color={colors.ltGray}>
+              February 3rd - 4th, 2022
             </Body>
-            <Body align="center" self="center" size="1.2rem">
+            <Body align="center" self="center" size="2rem" color={colors.ltGray}>
               <b>The Palmer House Hilton Hotel</b>
               <br />
-              <i>17 East Monroe Street Chicago, IL 60603</i>
+              <i>17 East Monroe Street<br />Chicago, IL 60603</i>
             </Body>
           </div>
         </CardContainer>
         <CardContainer
           bordered
           height="600px"
-          color={colors.fadedltGray}
+          color={colors.fadedPrimaryBlue}
           margins="1rem 0 0 1rem"
         >
-          <SubTitle size="2.5rem" margins="20px 0" self="center" align="center">
+          <SubTitle size="3.5rem" mobSize="1.9rem" margins="10px 0" self="center" align="center" line={0.9}>
             Don't Miss Out!
+          </SubTitle>
+          <SubTitle size="2rem" margins="10px 0" self="center" align="center">
+            Save these dates:
           </SubTitle>
           <div
             style={{
@@ -141,14 +155,14 @@ const InfoColumns = () => {
               justifyContent: "center",
             }}
           >
-            <Body align="center" self="center" size="1.2rem">
-              <b>September 1st 2021</b> - School Applications Open
+            <Body align="center" self="center" size="1.75rem">
+              <b>September 1st, 2021</b> - School Applications Open
             </Body>
-            <Body align="center" self="center" size="1.2rem">
-              <b>August 1st 2021</b> - Staff Applications Open
+            <Body align="center" self="center" size="1.75rem">
+              <b>August 1st, 2021</b> - Staff Applications Open
             </Body>
-            <Body align="center" self="center" size="1.2rem">
-              <b>January 16th 2022</b> - Position Papers Open
+            <Body align="center" self="center" size="1.75rem">
+              <b>January 16th, 2022</b> - Position Papers Submissions
             </Body>
           </div>
         </CardContainer>
