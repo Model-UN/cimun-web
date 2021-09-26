159640dfc36e2d79a8db11b79b602d107a844c59
sdmungov <stevenmun@yahoo.com>
2021-09-26 18:54:12 -0400

    school registration form

---
+++ pages/school-registration/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Head from "next/head";
+import HeaderNav from "../../components/headerNav";
+import Jumbotron from "../../components/jumbotron";
+import Footer from "../../components/footer";
+import SteppedForm from "../../components/stepped-form";
+import { Wrapper } from "../../styles/containers";
+
+const SchoolRegistration = () => {
+  return (
+    <Wrapper>
+      <Head>
+        <title>Chicago International Model UN</title>
+        <meta
+          name="description"
+          content="Chicago International Model United Nations"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish:wght@900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alegreya:wght@600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@500&family=Alegreya:wght@600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@500&family=Alegreya:wght@600&family=Source+Sans+Pro&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <HeaderNav />
+      <Jumbotron
+        titleOne="SCHOOL"
+        titleTwo="REGISTRATION"
+        height={65}
+        image="cimun-staff.png"
+      />
+      <SteppedForm confId="1" formId="2" />
+      <Footer />
+    </Wrapper>
+  );
+};
+
+export default SchoolRegistration;
