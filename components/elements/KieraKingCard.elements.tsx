b2121e2ca0b8f65b68c72fd40dcc99f41ca7ba3a
James He <28793743+he-james@users.noreply.github.com>
2022-11-01 19:59:23 -0600

    Update KieraKingCard.elements.tsx

--- components/elements/KieraKingCard.elements.tsx
+++ components/elements/KieraKingCard.elements.tsx
@@ -8,55 +8,53 @@ import React from "react";
 const AwardAnnouncementCard = () => {
   return (
     <ComponentWrapper margins={"auto"} pad={"0rem 6rem 0rem 6rem"}>
-      <Link href={"/scholarships/kiera-king"}>
-        <CardContainer margins="0" color={colors.plum} radius="8px">
-          <Body
-            align="start"
-            self="start"
-            margins="0 0 0.25rem 0"
-            size="2rem"
-            weight={900}
-            deco="underline"
-            color={colors.ivory}
-          >
-            Scholarship Opportunity for Delegates
-          </Body>
-          <SubTitle
-            size="2.5rem"
-            width="80%"
-            mobSize="1.5rem"
-            margins="10px 0"
-            self="start"
-            align="start"
-            line={0.9}
-            color={colors.ivory}
-          >
-            Apply for the Kiera King Delegate Award!
-          </SubTitle>
-          <Body
-            align="start"
-            self="start"
-            margins="0.25rem"
-            size="1.5rem"
-            weight={400}
-            color={colors.ivory}
-          >
-            The Kiera King Delegate Award is a delegate scholarship in the
-            memory of Kiera King, a dedicated Model UN delegate and former
-            member of Adlai E. Stevenson Model United Nations. This award aims
-            to recognize delegates with similar experiences so that they might
-            continue their competitive and professional legacies with an
-            acknowledgement of those who did not receive the same opportunities.
-            The winner of the award will receive $500 to support their education
-            or career after completing their high school education.
-            <br />
-            <br />
-            <strong>
-              <u>For more information, click here.</u>
-            </strong>
-          </Body>
-        </CardContainer>
-      </Link>
+      <CardContainer margins="0" color={colors.plum} radius="8px">
+        <Body
+          align="start"
+          self="start"
+          margins="0 0 0.25rem 0"
+          size="2rem"
+          weight={900}
+          deco="underline"
+          color={colors.ivory}
+        >
+          Scholarship Opportunity for Delegates
+        </Body>
+        <SubTitle
+          size="2.5rem"
+          width="80%"
+          mobSize="1.5rem"
+          margins="10px 0"
+          self="start"
+          align="start"
+          line={0.9}
+          color={colors.ivory}
+        >
+          Apply for the Kiera King Delegate Award!
+        </SubTitle>
+        <Body
+          align="start"
+          self="start"
+          margins="0.25rem"
+          size="1.5rem"
+          weight={400}
+          color={colors.ivory}
+        >
+          The Kiera King Delegate Award is a delegate scholarship in the
+          memory of Kiera King, a dedicated Model UN delegate and former
+          member of Adlai E. Stevenson Model United Nations. This award aims
+          to recognize delegates with similar experiences so that they might
+          continue their competitive and professional legacies with an
+          acknowledgement of those who did not receive the same opportunities.
+          The winner of the award will receive $500 to support their education
+          or career after completing their high school education.
+          <br />
+          <br />
+          <strong>
+            <u>For more information, contact us at <Link href="mailto:sc@cimun.org">sc@cimun.org.</Link></u>
+          </strong>
+        </Body>
+      </CardContainer>
     </ComponentWrapper>
   );
 };
