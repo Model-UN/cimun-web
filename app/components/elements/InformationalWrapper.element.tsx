b4cf999f842fb9ff8d9533f2c33b175ac3c29a8b
James He <28793743+he-james@users.noreply.github.com>
2022-11-30 18:30:51 -0700

    Update InformationalWrapper.element.tsx

--- app/components/elements/InformationalWrapper.element.tsx
+++ app/components/elements/InformationalWrapper.element.tsx
@@ -7,7 +7,7 @@ import {
 export const InformationalWrapper = ({ children }): JSX.Element => {
   return (
     <InfoColumnWrapper margins="0rem 1rem 0rem 1rem">
-      <CardWrapper flex="1" margins="1rem 15% 0 15%" pad="auto">
+      <CardWrapper flex="1" margins="0rem 15% 0 15%" pad="auto">
         <CardContainer noMargins>{children}</CardContainer>
       </CardWrapper>
     </InfoColumnWrapper>
