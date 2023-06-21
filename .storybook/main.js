437e3c939b96fb5a39b4365444bcb521ad31af42
Franco Reda <francoreda95@gmail.com>
2023-06-21 00:20:50 -0500

    Attempt to move to webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
 };
