32af7a5a39eb70fc2e90454e79d039f162bf0355
Franco Reda <francoreda95@gmail.com>
2023-06-21 01:27:46 -0500

    Storybook migrations

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    '../components/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", '../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
