1739801869927b00c34d729f1c35fa716ad0f6cd
Franco Reda <francoreda95@gmail.com>
2023-07-11 14:30:37 -0500

    I think this finally implements styled-components correctly that is

--- app/components/elements/MaterialCard.element.tsx
+++ app/components/elements/MaterialCard.element.tsx
@@ -38,7 +38,9 @@ export const MaterialCard = styled.div<MaterialCardProps>`
     props.background ? props.background : "#fff"};
   border-radius: 2px;
 
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow:
+    0 1px 3px rgba(0, 0, 0, 0.12),
+    0 1px 2px rgba(0, 0, 0, 0.24);
 
   transition: ${(props: MaterialCardProps) => {
     return props.$noAnimate ? "none" : "all 0.3s cubic-bezier(.25,.8,.25,1)";
